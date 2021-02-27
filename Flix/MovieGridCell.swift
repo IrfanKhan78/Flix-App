1092392243e3116a8723c27b74da400388f95c1f
Irfan Khan <imk5134@psu.edu>
2021-02-28 00:46:31 +0600

    Unit 1 part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Irfan Mahir Khan on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
